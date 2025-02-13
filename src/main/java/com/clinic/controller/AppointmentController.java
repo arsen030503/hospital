8b21c41032f46087195d48c93dc1065fa77be8c6
arsen <arsen.suranov2003@gmail.com>
2025-02-13 12:58:36 +0600

    init

--- src/main/java/com/clinic/controller/AppointmentController.java
+++ src/main/java/com/clinic/controller/AppointmentController.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/appointments")
 public class AppointmentController {
+    //kj nknkj
     private final AppointmentService appointmentService;
 
     // ✅ Добавлен конструктор
